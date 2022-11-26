9e4425a9e78d236b1086c6cb5d00582ff769a169
Pierre <bonnamy.77@gmail.com>
2022-11-26 20:26:45 +0100

    checkhealth

--- pages/index.vue
+++ pages/index.vue
@@ -1,3 +1,24 @@
 <template>
   <h1>Index page</h1>
 </template>
+<script>
+export default {
+  name: 'Index',
+  data() {
+    return {
+    };
+  },
+  mounted() {
+    this.checkHealth();
+  },
+  methods: {
+    async checkHealth() {  
+      const response = await $fetch( 'http://127.0.0.1:8000/api/health', {
+        method: 'GET',
+      });
+
+      console.log(response);
+    }
+  },
+};
+</script>
